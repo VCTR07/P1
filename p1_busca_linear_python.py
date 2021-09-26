d70e8dcd3f7407632c0bbfc505da56433c824ee0
VCTR07 <victor_emanuell@yahoo.com.br>
2021-09-26 17:47:58 -0300

    Fixando o valor de x

--- p1_busca_linear_python.py
+++ p1_busca_linear_python.py
@@ -18,11 +18,30 @@ def busca_linear_melhorada(A, n, x):
             break
     return resposta
 
+def busca_linear_sentinela(A, n, x):
+    resposta = -1
+
+    ultimo = A[n-1]
+    A[n-1] = x
+    i = 0
+
+    while A[i] != x:
+        i+=1
+
+    A[n-1] = ultimo
+
+    if i < n-1 or A[n-1] == x:
+        resposta = i
+
+    return resposta
+
 #Entardas
 A=list(range(0,10000)) #cria uma lista com elementos de 0 a 10.000
 random.shuffle(A) # embaralha a lista criada
 n=len(A)
-x=random.randrange(0, 10000) # escolhe um número entre 0 e 10.0000
+#x=random.randrange(0, 10000) # escolhe um número entre 0 e 10.0000
+x= 5758
+
 
 #Saída Busca Linear
 inicia_tempo = time.time_ns()
@@ -33,7 +52,14 @@ print(f"Na BL, para o número {x} temos a resposta {saida}, com tempo de execuç
 
 #Saída Busca Linear Melhorada
 inicia_tempo = time.time_ns()
-saida = busca_linear(A, n, x)
+saida = busca_linear_melhorada(A, n, x)
 termina_tempo=time.time_ns()
 tempo_total = termina_tempo - inicia_tempo
 print(f"Na BLM, para o número {x} temos a resposta {saida}, com tempo de execução em ns de {tempo_total}.")
+
+#Saída Busca Linear com Sentinela
+inicia_tempo = time.time_ns()
+saida = busca_linear_sentinela(A, n, x)
+termina_tempo=time.time_ns()
+tempo_total = termina_tempo - inicia_tempo
+print(f"Na BLS, para o número {x} temos a resposta {saida}, com tempo de execução em ns de {tempo_total}.")
